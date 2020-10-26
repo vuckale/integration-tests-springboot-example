c3d5f159749e9f846be764c89a76fa020060f3ff
vuckale <56733276+vuckale@users.noreply.github.com>
2020-10-26 20:53:36 +0100

    removed unnecessary DB transaction setup and teardown

--- src/test/java/at/ac/tuwien/sepm/assignment/individual/integration/OwnerEndpointTest.java
+++ src/test/java/at/ac/tuwien/sepm/assignment/individual/integration/OwnerEndpointTest.java
@@ -6,8 +6,6 @@ import at.ac.tuwien.sepm.assignment.individual.entity.Owner;
 import at.ac.tuwien.sepm.assignment.individual.service.OwnerService;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,15 +19,10 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionDefinition;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assumptions.assumeTrue;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
@@ -41,29 +34,10 @@ public class OwnerEndpointTest {
     MockMvc mockMvc;
 
     @Autowired
-    PlatformTransactionManager txm;
-
-    TransactionStatus txstatus;
-
-    @Autowired
     OwnerService ownerService;
 
     @Autowired
     private ObjectMapper objectMapper;
-
-    @BeforeEach
-    public void setupDBTransaction() {
-        DefaultTransactionDefinition def = new DefaultTransactionDefinition();
-        def.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW);
-        txstatus = txm.getTransaction(def);
-        assumeTrue(txstatus.isNewTransaction());
-        txstatus.setRollbackOnly();
-    }
-
-    @AfterEach
-    public void tearDownDBData() {
-        txm.rollback(txstatus);
-    }
 
     // GET reuqest
     @Test
